0b3f1af0c37e4d065837666dfbd693c25c8874de
Tapiwanashe Maposhere <maposheret@yahoo.com>
2020-12-13 21:32:06 +0200

    vaidation done

--- lib/auth/SignUp.dart
+++ lib/auth/SignUp.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:localregex/localregex.dart';
 import 'package:ndialog/ndialog.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:token_app/api/api.dart';
@@ -28,6 +29,9 @@ class SignUpState extends State<SignUp> {
   TextEditingController number = new TextEditingController();
   TextEditingController password = new TextEditingController();
   TextEditingController cpassword = new TextEditingController();
+  String _error = "";
+  final localregex = LocalRegex();
+  bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -93,6 +97,22 @@ class SignUpState extends State<SignUp> {
                 SizedBox(
                   height: spacing_xlarge,
                 ),
+                Text(
+                  _error,
+                  style:
+                      TextStyle(color: Colors.red, fontStyle: FontStyle.italic),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                _isLoading
+                    ? CircularProgressIndicator()
+                    : SizedBox(
+                        width: 0.0,
+                      ),
+                SizedBox(
+                  height: 10.0,
+                ),
                 AppButton(
                   onPressed: () {
                     if (name.text != "" &&
@@ -101,25 +121,60 @@ class SignUpState extends State<SignUp> {
                         password.text != "" &&
                         cpassword.text != "") {
                       setState(() {
+                        _isLoading = true;
                         progressDialog.show();
                       });
 
-                      API
-                          .register(name.text, email.text, number.text,
-                              password.text, cpassword.text)
-                          .then((response) {
-                        User user = User.fromJson(json.decode(response.body));
-
-                        if (user.id != null) {
-                          saveUserPrefs(user);
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => HomePage()),
-                          );
-                        } else {}
-                      });
+                      if (localregex.isZwMobile(number.text)) {
+                        if (password.text.trim() == cpassword.text.trim()) {
+                          if (localregex.isEmail(email.text)) {
+                            setState(() {
+                              _error = "";
+                            });
+                            API
+                                .register(name.text, email.text, number.text,
+                                    password.text, cpassword.text)
+                                .then((response) {
+                              try {
+                                User user =
+                                    User.fromJson(json.decode(response.body));
+                                saveUserPrefs(user);
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => HomePage()),
+                                );
+                              } on FormatException catch (e) {
+                                print('error caught: $e');
+                                setState(() {
+                                  _isLoading = false;
+                                  _error = "Server error. Try again";
+                                });
+                              }
+                            });
+                          } else {
+                            setState(() {
+                              _isLoading = false;
+                              _error = "Invalid email address";
+                            });
+                          }
+                        } else {
+                          setState(() {
+                            _isLoading = false;
+                            _error = "Password Mismatch.";
+                          });
+                        }
+                      } else {
+                        setState(() {
+                          _isLoading = false;
+                          _error = "Invalid phone number";
+                        });
+                      }
                     } else {
-                      print("error");
+                      setState(() {
+                        _isLoading = false;
+                        _error = "No empty fields allowed";
+                      });
                     }
                     progressDialog.dismiss();
                   },
